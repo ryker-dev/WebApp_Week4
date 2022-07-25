6ae043f96c9140b41130c75ddeb87c3340e3dbcb
ryker-dev <pizzuti.dev@gmail.com>
2022-07-26 02:45:50 +0300

    Fixed response

--- routes/images.js
+++ routes/images.js
@@ -2,8 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.post('/', function(req, res, next) {
-    const form = FormData;
-  res.render('index', { title: 'Express' });
+  res.send("Hi")
 });
 
 module.exports = router;
