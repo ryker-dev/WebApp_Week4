b8fa9d88e8bc9599b43b9b06e7b1d609895c16d6
ryker-dev <pizzuti.dev@gmail.com>
2022-07-26 02:32:29 +0300

    changed port because of CodeGrade

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,7 +1,7 @@
 //const port = process.env.PORT || '3000';
 const ingredientList = [];
 const instructionList = [];
-const port = 3000;
+const port = 1234;
 
 async function fetchRecipe (recipeName) {
     const response = await fetch(`http://localhost:${port}/recipe/pizza`);
