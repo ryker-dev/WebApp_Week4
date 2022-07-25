a80436da21fbaad0d716c5ea0bc70709a7fd0e01
ryker-dev <pizzuti.dev@gmail.com>
2022-07-26 02:40:08 +0300

    fixed typo

--- app.js
+++ app.js
@@ -19,6 +19,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/recipe', recipeRouter);
-app.use('/image', imageRouter);
+app.use('/images', imageRouter);
 
 module.exports = app;
